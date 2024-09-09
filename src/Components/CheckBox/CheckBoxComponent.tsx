56e10cd7928446576e5cb5748b8f15bf44827046
ronaldAlhazov <ronalda@moveohls.com>
2024-09-09 16:19:01 +0300

    start to work on checkbox

--- src/Components/CheckBox/CheckBoxComponent.tsx
+++ src/Components/CheckBox/CheckBoxComponent.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export const CheckBoxComponent = () => {
+const CheckBoxComponent = () => {
   return <div>CheckBoxComponent</div>;
 };
+
+export default CheckBoxComponent;
