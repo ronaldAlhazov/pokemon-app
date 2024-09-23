1f33355fedf0f9af7306b3459530cad4ebeb86c6
ronaldAlhazov <ronalda@moveohls.com>
2024-09-23 09:57:21 +0300

    headerFightArenaDone

--- src/Components/DropDown/utils.tsx
+++ src/Components/DropDown/utils.tsx
@@ -1,6 +1,9 @@
-import { TextField, InputAdornment, Box } from "@mui/material";
+import { TextField, InputAdornment, Box, Theme, Popper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import { DropDownType } from "./consts";
+import Typography from "../Typography/Typography";
+import { TypographyTypes } from "../Typography/consts";
+import { useEffect } from "react";
 
 export const renderInput = (
   type: DropDownType,
@@ -14,6 +17,15 @@ export const renderInput = (
       {...params}
       label={label}
       sx={style}
+      InputLabelProps={{
+        style: {
+          fontFamily: "Roboto",
+          fontSize: "14px",
+          fontWeight: 400,
+          lineHeight: "22px",
+          textAlign: "center",
+        },
+      }}
       variant="outlined"
       inputProps={{ ...params.inputProps, readOnly: true }}
     />
@@ -38,21 +50,65 @@ export const renderInput = (
 };
 export const renderOption = (props: any, option: any) => {
   const { key, ...optionProps } = props;
+
   return (
     <Box
       key={key}
       component="li"
-      sx={{ "& > img": { mr: 2, flexShrink: 0 } }}
+      sx={{
+        display: "flex",
+        alignItems: "center",
+      }}
       {...optionProps}
     >
       {option.image && (
-        <img
-          src={option.image}
-          alt={option.label}
-          style={{ width: 24, height: 24, borderRadius: "50%" }}
-        />
+        <Box
+          sx={{
+            width: 160,
+            display: "flex",
+            alignItems: "center",
+            pr: 2,
+            "& > img": {
+              flexShrink: 0,
+              paddingRight: "4px",
+            },
+          }}
+        >
+          <img
+            src={option.image}
+            alt={option.label}
+            style={{ width: 24, height: 24, borderRadius: "50%" }}
+          />
+          <Typography
+            type={TypographyTypes.BODY_REGULAR}
+            label={option.value}
+          />
+        </Box>
       )}
-      {option.label} {option.value}
+
+      <Typography type={TypographyTypes.BODY_REGULAR} label={option.label} />
     </Box>
+  );
+};
+const styles = (theme: Theme) => ({
+  popper: {
+    maxWidth: "fit-content",
+  },
+});
+
+type OptionType = {
+  title: string;
+};
+
+interface PopperMyProps {}
+
+export const PopperMy: React.FC<PopperMyProps> = (props) => {
+  return (
+    <Popper
+      open={false}
+      {...props}
+      style={styles({} as Theme).popper}
+      placement="bottom-start"
+    />
   );
 };
---
+++ src/Pages/FightArena/Components/Header/styels.ts
@@ -0,0 +1,19 @@
+import { colors } from "../../../../global-styles";
+
+export const getDropdownStyle = (sortByLength: number) => ({
+  width: 200,
+  height: 38,
+  paddingBottom: "16px",
+});
+export const getTypographyStyle = () => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "8px",
+  textAlign: "center",
+});
+export const getFooterStyle = () => ({
+  display: "flex",
+  justifyContent: "flex-start",
+  width: "100%",
+});
